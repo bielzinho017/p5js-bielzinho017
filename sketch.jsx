e653ccc8233c1e34e0d8fc75347217a20cfaddbf
bielzinho017 <caparrozgabriel9@gmail.com>
2024-05-27 20:18:45 -0300

    sketch.jsx

--- sketch.jsx
+++ sketch.jsx
@@ -1,30 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+//variáveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 20;
+let raio = diametro / 2;
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</ht
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
